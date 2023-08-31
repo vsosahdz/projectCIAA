a1173a5f8325a560afa6f9d3f43fe5705f722da8
Víctor Adrián <victoradrian@Administrators-MacBook-Air-4.local>
2023-08-31 17:32:32 -0600

    update 3

--- servidor.py
+++ servidor.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify,request
+import numpy as np
+from joblib import load
+import os
 
+#Cargar el modelo
+dt=load('dt1.joblib')
+
+#Servidor (backend)
 servidorWeb = Flask(__name__)
-
 
 @servidorWeb.route("/holamundo",methods=['GET'])
 def formulario():
     return render_template('pagina1.html')
+
+#Envio de datos a través de JSON
+@servidorWeb.route('/modelo',methods=['POST'])
+def modeloPrediccion():
+    #Procesar los datos de entrada
+    contenido = request.json
+    print(contenido)
+    return jsonify({'resultado':"Hola"})
+
 
 if __name__ == '__main__':
     servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
