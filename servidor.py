ed7551132f7745836b77a17e887c27680d053ca6
Víctor Adrián <victoradrian@Administrators-MacBook-Air-4.local>
2023-09-06 19:37:55 -0600

    front

--- servidor.py
+++ servidor.py
@@ -29,6 +29,21 @@ def modeloPrediccion():
     resultado=dt.predict(datosEntrada.reshape(1,-1))
     return jsonify({'resultado':str(resultado[0])})
 
+@servidorWeb.route('/modeloForm',methods=['POST'])
+def modeloForm():
+    #Procesar los datos de entrada
+    contenido = request.form
+    print(contenido)
+    datosEntrada = np.array([ 
+        0.88,	0,	2.6,	0.098,	25,	67,	0.9968, 1, 0.4,
+        contenido['pH'],
+        contenido['sulphates'],
+        contenido['alcohol']
+    ])
+    #Utilizar el modelo
+    resultado=dt.predict(datosEntrada.reshape(1,-1))
+    return jsonify({'resultado':str(resultado[0])})
+
 
 if __name__ == '__main__':
     servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
